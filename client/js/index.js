649be95b99d8d59f4381d7bb6e02df95e4696309
adis7102 <adis7102@gmail.com>
2019-07-11 21:38:30 +0700

    bikin login logout

--- client/js/index.js
+++ client/js/index.js
@@ -2,6 +2,8 @@ const baseUrl= 'http://localhost:3000'
 
 $( document ).ready(function() {
     console.log( "ready!" );
+      isLogin()
+
     $('#to-login').click(function(event) {
       console.log('masukkkk')
       event.preventDefault()
@@ -16,8 +18,67 @@ $( document ).ready(function() {
       $('#login').hide()
     })
 
+    $('#register-form').submit(function(event) {
+      // event.preventDefault()
+      let newData = {
+        name : $('#name-reg').val(),
+        email : $('#email-reg').val(),
+        password : $('#password-reg').val()
+      }
+      register(newData)
+    })
+
+    $('#login-form').submit(function(event) {
+      event.preventDefault()
+      let userData = {
+        email : $('#email-log').val(),
+        password : $('#password-log').val()
+      }
+      console.log(userData)
+      login(userData)
+    })
+    $('#to-logout').click(function(event) {
+      event.preventDefault()
+      logout()
+    })
 });
 
+function register (newUser) {
+  $.ajax({
+    url: `${baseUrl}/users/register`,
+    type: 'post',
+    dataType: 'json',
+    data: newUser
+  })
+    .done(function(success){
+        console.log(success)
+        let loginOption = {
+          email : newUser.email,
+          password: newUser.password
+        }
+        login(loginOption)
+      })
+    .fail(function(error){
+        console.log(error)
+    })
+}
+
+function login (loginOption) {
+  $.ajax({
+    url: `${baseUrl}/users/login`,
+    type: 'post',
+    dataType: 'json',
+    data: loginOption
+  })
+    .done(function(Data){
+      console.log(Data)
+      localStorage.setItem('token', Data.token)
+      localStorage.setItem('name', Data.name)
+    })
+    .fail(function(error){
+      console.log(error)
+    })
+}
 
 function onSignIn(googleUser) {
   console.log('masuk google sign in')
@@ -31,13 +92,49 @@ function onSignIn(googleUser) {
      .done(function(Data){
        console.log(Data)
        localStorage.setItem('token', Data.token)
-       localStorage.setItem('firstName', Data.firstName)
-       localStorage.setItem('lastName', Data.lastName)
-       localStorage.setItem('userId', Data.id)
+       localStorage.setItem('name', Data.name)
        hasToken()
      })
      .fail(function(err){
       console.log(err)
   
      })
+}
+
+function isLogin(){
+  if(localStorage.token){
+    hasToken()
+  }else{
+    noToken()
   }
+}
+
+function hasToken(){
+    $('#to-login').hide()
+    $('#to-register').hide()
+    $('#register').hide()
+    $('#login').hide()
+    $('#to-logout').show()
+}
+
+function noToken(){
+  $('#to-login').show()
+  $('#to-register').show()
+  $('#register').hide()
+  $('#login').show()
+  $('#to-logout').hide()
+}
+
+function logout(){
+  localStorage.clear()
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut()
+  .then(function(){
+    console.log('User signed out')
+    noToken()
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+  noToken()
+}
