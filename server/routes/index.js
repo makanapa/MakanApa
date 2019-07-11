efc2e880650cafe531bcedfea6fc307fcfdf0079
Luqman <luqman.zakariya@gmail.com>
2019-07-11 21:17:18 +0700

    nutritions backend done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router= require('express').Router()
 const userRoute= require('./user')
+const nutritionRoute = require('./nutrition')
 
 router.use('/users', userRoute)
+router.use('/nutritions', nutritionRoute)
 
 module.exports= router
