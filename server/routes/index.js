f1b86ca6f63563b6f6e250a206faead69fd9bc2e
Luqman <luqman.zakariya@gmail.com>
2019-07-12 10:17:12 +0700

    add wheather

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ const router= require('express').Router()
 const userRoute= require('./user')
 const nutritionRoute = require('./nutrition')
 const restoRoute= require('./resto')
+const wheatherRoute = require('./wheather')
 
 router.use('/users', userRoute)
 router.use('/nutritions', nutritionRoute)
 router.use('/resto', restoRoute)
+router.use('/wheathers', wheatherRoute)
 
 module.exports= router
